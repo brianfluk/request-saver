a83b22a7f4eca62602bf54df5daeb56a8604737c
Brian-Luk <brian.luk@ibm.com>
2019-02-25 13:56:02 -0500

    UI, code readability

--- popup.js
+++ popup.js
@@ -7,12 +7,55 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 })
 
+document.getElementById('targetURL').addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("targetURLBtn").click();
+    }
+});
+document.getElementById('mockServerURL').addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("mockServerURLBtn").click();
+    }
+});
+
+function transitionSaved(id) {
+    document.getElementById(id).classList.remove('transparent')
+    setTimeout(function () {
+        document.getElementById(id).classList.add('transparent')
+    }, 500)
+}
+
+document.getElementById('targetURLBtn').addEventListener('click', function() {
+    chrome.storage.sync.get('targetURL', function(data) {
+        console.log('target url before update:', data.targetURL);
+    });
+    let targetURL = document.getElementById('targetURL').value
+    if (targetURL) {
+        chrome.storage.sync.set({ 'targetURL': targetURL }, function() {
+            transitionSaved('targetSaved')
+        });
+    }
+})
+
+document.getElementById('mockServerURLBtn').addEventListener('click', function() {
+    chrome.storage.sync.get('mockServerURL', function(data) {
+        console.log('mock server before update:', data.mockServerURL);
+    });
+    let mockServerURL = document.getElementById('mockServerURL').value
+    if (mockServerURL) {
+        chrome.storage.sync.set({ 'mockServerURL': mockServerURL }, function() {
+            transitionSaved('mockServerSaved')
+        });
+    }
+})
+
 if (chrome.extension.getBackgroundPage().enabled == false) {
     document.getElementById('disableBtn').setAttribute("class", "hidden")
 } else {
     document.getElementById('enableBtn').setAttribute("class", "hidden")
 }
-
 
 document.getElementById('enableBtn').addEventListener('click', function() {
     chrome.extension.getBackgroundPage().enabled = true;
@@ -23,23 +66,4 @@ document.getElementById('disableBtn').addEventListener('click', function() {
     chrome.extension.getBackgroundPage().enabled = false;
     document.getElementById('disableBtn').setAttribute("class", "hidden")
     document.getElementById('enableBtn').removeAttribute("class", "hidden")
-})
-
-document.getElementById('mockServerURLBtn').addEventListener('click', function() {
-    chrome.storage.sync.get('mockServerURL', function(data) {
-        console.log('mock server before update:', data.mockServerURL);
-    });
-    let mockServerURL = document.getElementById('mockServerURL').value
-    if (mockServerURL) {
-        chrome.storage.sync.set({ 'mockServerURL': mockServerURL });
-    }
-})
-document.getElementById('targetURLBtn').addEventListener('click', function() {
-    chrome.storage.sync.get('targetURL', function(data) {
-        console.log('target url before update:', data.targetURL);
-    });
-    let targetURL = document.getElementById('targetURL').value
-    if (targetURL) {
-        chrome.storage.sync.set({ 'targetURL': targetURL });
-    }
 })
